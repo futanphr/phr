45200236dd421012744e47a340c509aec71f2ef2
penghuari@9fbank.com.cn <penghuari@9fbank.com.cn>
2018-07-30 09:31:13 +0800

    启动类修改

--- phr-rest/src/main/java/com/phr/rest/ApplicationStart.java
+++ phr-rest/src/main/java/com/phr/rest/ApplicationStart.java
@@ -1,9 +1,12 @@
 package com.phr.rest;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -14,11 +17,20 @@ import com.phr.common.redis.SpringRedisCacheManager;
 import com.phr.rest.biz.config.RedisConfig;
 import com.phr.rest.config.ApplicationConfig;
 import com.phr.rest.mybatis.shardingjdbc.config.DataSourceConfig;
+import com.phr.rest.mybiz.CountNumber;
 
 
 public class ApplicationStart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(ApplicationConfig.class, args);
+		/* ConfigurableApplicationContext context = SpringApplication.run(ApplicationConfig.class, args);
+
+	        @Async和@EnableAsync配合使用
+	    context.getBean(CountNumber.class).PrintNumber();
+	        for(int i=1; i<10; i++){
+	            TimeUnit.MICROSECONDS.sleep(1);
+	            System.out.println("------------------");
+	        }*/
 	}
 }
